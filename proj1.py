b6b79944d6059d5599b99b4ee1a6a745e47ffc9c
Hjpnam <hjpnam@gmail.com>
2018-02-05 11:46:20 -0600

    Basic noun chunking added for person's name recognition

--- proj1.py
+++ proj1.py
@@ -1,10 +1,43 @@
 import textblob as tb
+from nltk import *
+from nltk.corpus import stopwords
 import json
 import os
 import re
 
-data = json.load(open('gg2018.json'))
+stop = stopwords.words('english')
 
-for i in range(100):
-	
-gi	print re.search(data[i]['text'], "bestdirector", flags=re.I)
+data = json.load(open('gg2018.json'))
+strdata = "Allison Sun best director 2018 Golden Globes"
+'''
+for i in range(5):
+	b = tb.TextBlob(data[i]["text"])
+	if ("my son" in b.lower())
+		print i
+'''
+'''
+def ie_preprocess(document):
+    document = ' '.join([i for i in document.split() if i not in stop])
+    sentences = nltk.sent_tokenize(document)
+    sentences = [nltk.word_tokenize(sent) for sent in sentences]
+    sentences = [nltk.pos_tag(sent) for sent in sentences]
+    return sentences
+		
+		
+def extract_names(document):
+		names = []
+		sentences = ie_preprocess(document)
+		for tagged_sentence in sentences:
+			for chunk in nltk.ne_chunk(tagged_sentence):
+				if type(chunk) == nltk.tree.Tree:
+					if chunk.label() == 'PERSON':
+						names.append(' '.join([c[0] for c in chunk]))
+					if chunk.label() == "ORGANIZATION":
+						names.append(' '.join([c[0] for c in chunk]))
+		return names
+		'''
+
+tagged = pos_tag(strdata.lower().split())
+names = [word for word,pos in tagged if pos == "NNP"]
+
+print names
