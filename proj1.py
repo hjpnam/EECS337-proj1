3542f15ca1425287259083a88b51c8b87badd4ac
allisonsun <allisunson@gmail.com>
2018-02-07 00:10:32 -0600

    get first names

--- proj1.py
+++ proj1.py
@@ -1,43 +1,28 @@
 import textblob as tb
-from nltk import *
+import nltk
 from nltk.corpus import stopwords
+from nltk.corpus import names
 import json
 import os
 import re
 
 stop = stopwords.words('english')
-
 data = json.load(open('gg2018.json'))
-strdata = "Allison Sun best director 2018 Golden Globes"
-'''
-for i in range(5):
-	b = tb.TextBlob(data[i]["text"])
-	if ("my son" in b.lower())
-		print i
-'''
-'''
-def ie_preprocess(document):
-    document = ' '.join([i for i in document.split() if i not in stop])
-    sentences = nltk.sent_tokenize(document)
-    sentences = [nltk.word_tokenize(sent) for sent in sentences]
-    sentences = [nltk.pos_tag(sent) for sent in sentences]
-    return sentences
-		
-		
-def extract_names(document):
-		names = []
-		sentences = ie_preprocess(document)
-		for tagged_sentence in sentences:
-			for chunk in nltk.ne_chunk(tagged_sentence):
-				if type(chunk) == nltk.tree.Tree:
-					if chunk.label() == 'PERSON':
-						names.append(' '.join([c[0] for c in chunk]))
-					if chunk.label() == "ORGANIZATION":
-						names.append(' '.join([c[0] for c in chunk]))
-		return names
-		'''
+strdata = "Allison Sun Meryl Streep brad pitt chris evans Golden Globes sameena william peter"
 
-tagged = pos_tag(strdata.lower().split())
-names = [word for word,pos in tagged if pos == "NNP"]
+def get_names(str):
+    # currently only gets first names
+	names = nltk.corpus.names
+	all_names = names.words('male.txt') + names.words('female.txt')
+	all_names = [name.lower() for name in all_names]
 
-print names
+	tweet_names= []
+	words = str.lower().split()
+
+	for i in range(0, len(words)):
+		if words[i] in all_names:
+			tweet_names.append(words[i])
+	print tweet_names
+	return tweet_names
+
+get_names(strdata)
