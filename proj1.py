116d32e1ac2e833d473aa48326a823aa82d529e6
Hjpnam <hjpnam@gmail.com>
2018-02-07 20:28:18 -0600

    process_tweet added, readme updated

--- proj1.py
+++ proj1.py
@@ -1,43 +1,23 @@
-import textblob as tb
-from nltk import *
+import nltk
 from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 import json
-import os
 import re
+from difflib import SequenceMatcher
 
-stop = stopwords.words('english')
+OFFICIAL_AWARDS = ['cecil b. demille award', 'best motion picture - drama', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best motion picture - comedy or musical', 'best performance by an actress in a motion picture - comedy or musical', 'best performance by an actor in a motion picture - comedy or musical', 'best animated feature film', 'best foreign language film', 'best performance by an actress in a supporting role in a motion picture', 'best performance by an actor in a supporting role in a motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best television series - comedy or musical', 'best performance by an actress in a television series - comedy or musical', 'best performance by an actor in a television series - comedy or musical', 'best mini-series or motion picture made for television', 'best performance by an actress in a mini-series or motion picture made for television', 'best performance by an actor in a mini-series or motion picture made for television', 'best performance by an actress in a supporting role in a series, mini-series or motion picture made for television', 'best performance by an actor in a supporting role in a series, mini-series or motion picture made for television']
 
 data = json.load(open('gg2018.json'))
-strdata = "Allison Sun best director 2018 Golden Globes"
-'''
-for i in range(5):
-	b = tb.TextBlob(data[i]["text"])
-	if ("my son" in b.lower())
-		print i
-'''
-'''
-def ie_preprocess(document):
-    document = ' '.join([i for i in document.split() if i not in stop])
-    sentences = nltk.sent_tokenize(document)
-    sentences = [nltk.word_tokenize(sent) for sent in sentences]
-    sentences = [nltk.pos_tag(sent) for sent in sentences]
-    return sentences
-		
-		
-def extract_names(document):
-		names = []
-		sentences = ie_preprocess(document)
-		for tagged_sentence in sentences:
-			for chunk in nltk.ne_chunk(tagged_sentence):
-				if type(chunk) == nltk.tree.Tree:
-					if chunk.label() == 'PERSON':
-						names.append(' '.join([c[0] for c in chunk]))
-					if chunk.label() == "ORGANIZATION":
-						names.append(' '.join([c[0] for c in chunk]))
-		return names
-		'''
 
-tagged = pos_tag(strdata.lower().split())
-names = [word for word,pos in tagged if pos == "NNP"]
-
-print names
+def process_tweet(tweet):
+	tweet = re.sub(r"http\S+", "", tweet)
+	tweet = re.sub(r"#\S+", "", tweet)
+	tweet = re.sub(r"@\S+", "", tweet)
+	word_tokens = word_tokenize(tweet)
+	stop_words = Set(stopwords.words('english')) | Set(["GoldenGlobes", 'goldenglobes', 'Goldenglobes', 'Golden','golden','globes','Globes', 'RT', 'I', "Oscars", "oscars", "Awards", "awards","!",",",".","?",":",';'])
+	
+	filtered = [w for w in word_tokens if not w in stop_words]
+	return filtered
+	
+def get_awards():
+	pass
