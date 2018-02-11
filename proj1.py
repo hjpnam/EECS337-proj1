9e6e9eb5839859405921a73f9b91e6c61f4e0b11
allisonsun <allisunson@gmail.com>
2018-02-11 15:48:35 -0600

    wip

--- proj1.py
+++ proj1.py
@@ -10,8 +10,8 @@ import wikipedia
 
 OFFICIAL_AWARDS = ['cecil b. demille award', 'best motion picture - drama', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best motion picture - comedy or musical', 'best performance by an actress in a motion picture - comedy or musical', 'best performance by an actor in a motion picture - comedy or musical', 'best animated feature film', 'best foreign language film', 'best performance by an actress in a supporting role in a motion picture', 'best performance by an actor in a supporting role in a motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best television series - comedy or musical', 'best performance by an actress in a television series - comedy or musical', 'best performance by an actor in a television series - comedy or musical', 'best mini-series or motion picture made for television', 'best performance by an actress in a mini-series or motion picture made for television', 'best performance by an actor in a mini-series or motion picture made for television', 'best performance by an actress in a supporting role in a series, mini-series or motion picture made for television', 'best performance by an actor in a supporting role in a series, mini-series or motion picture made for television']
 
-data = json.load(open('gg2018.json'))
-data = [tweet['text'] for tweet in data]
+# data = json.load(open('gg2018.json'))
+# data = [tweet['text'] for tweet in data]
 
 tokenized_data = ["Allison", "Sun", "Meryl", "Streep", "brad", "pitt", "chris", "evans", "Golden", "Globes" "sameena", "william", "peter"]
 
@@ -48,5 +48,38 @@ def get_names(words):
 			else:
 				tweet_names.append(first_name)
 	return tweet_names
+# print get_names(tokenized_data)
 
-print get_names(tokenized_data)
+
+def filter_tweets(tweets, keywords):
+	keyword_tweets = []
+	for tweet in tweets:
+		for word in keywords:
+			if word in tweet:
+				keyword_tweets.append(tweet)
+	return keyword_tweets
+
+def get_nominees(tweets):
+	awards_nominees = {}
+	awards_nominees_tweets = {}
+	nominee_tweets = []
+    nominee_words = ['nominee', 'nominees', 'nominating', 'nominated', 'nominates', 'Nominee', 'Nominees', 'Nominating', 'Nominated', 'Nominates']
+
+	for award in OFFICIAL_AWARDS:
+		awards_nominees[award] = []
+		awards_nominees_tweets[award] = []
+
+	nominee_tweets = filter_tweets(tweets, nominee_words)
+
+    # go through nominee tweets, if any award is in a nominee tweet, add that nominee tweet to dictionary
+	for nominee_tweet in nominee_tweets:
+		if any(word in nominee_tweet for award in OFFICIAL_AWARDS):
+			awards_nominees_tweets[award].append(nominee_tweet)
+
+	# values of dict are tweets, want to clean up so only names remain
+	for key, values in award_nominees_tweets.iteritems():
+		for value in values:
+			awards_nominees[award].append(get_names(value))
+
+	for key, values in award_nominees.iteritems():
+        # get 5 most common
