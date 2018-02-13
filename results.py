b221c3905a886422d77a4dce8d1cd19c9cd8c333
allisonsun <allisunson@gmail.com>
2018-02-13 11:55:45 -0600

    get host working

--- results.py
+++ results.py
@@ -11,45 +11,43 @@ from collections import Counter
 data = json.load(open('gg2018.json'))
 data = [tweet['text'] for tweet in data]
 
-# def get_people_names(words):
-#     # input: array of strings
-# 	names = nltk.corpus.names
-# 	all_names = names.words('male.txt') + names.words('female.txt')
-# 	all_names = [name.lower() for name in all_names]
-#
-# 	tweet_names= []
-#
-# 	for i in range(0, len(words)):
-# 		if words[i].lower() in all_names:
-# 			first_name = words[i]
-# 			if i+1 < len(words):
-# 				last_name = words[i+1]
-# 				full_name = first_name.title() + " " + last_name.title()
-# 				if full_name in wikipedia.search(full_name) or full_name + " (actor)" in wikipedia.search(full_name):
-# 					tweet_names.append(full_name)
-# 			else:
-# 				tweet_names.append(first_name)
-# 	return tweet_names
+def remove_duplicates(tweets):
+    return list(set(tweets))
 
-def get_people_names(tweet):
-    # input: string
-    names = nltk.corpus.names
-    all_names = names.words('male.txt') + names.words('female.txt')
-    all_names = [name.lower() for name in all_names]
-    words = tweet.split()
-    tweet_names= []
+def process_tweet(tweet):
+	tweet = re.sub(r"http\S+", "", tweet)
+	#tweet = re.sub(r"#\S+", "", tweet)
+	#tweet = re.sub(r"@\S+", "", tweet)
+	word_tokens = word_tokenize(tweet)
+	stop_words = Set(stopwords.words('english')) | Set(["GoldenGlobes", 'goldenglobes', 'Goldenglobes', 'Golden','golden','globes','Globes', 'RT', 'I', "Oscars", "oscars", "Awards", "awards","!",",",".","?",":",';',"#","@"])
 
-    for i in range(len(words)):
-        if words[i].lower() in all_names:
-            first_name = words[i]
-            if i+1 < len(words):
-                last_name = words[i+1]
-                full_name = first_name.title() + " " + last_name.title()
-                if full_name in wikipedia.search(full_name) or full_name + " (actor)" in wikipedia.search(full_name):
-                    tweet_names.append(full_name)
-            else:
-                tweet_names.append(first_name)
-    return tweet_names
+	filtered = [w for w in word_tokens if not w in stop_words]
+	return filtered
+
+tweets = remove_duplicates(data[0:20000])
+processed_tweets = []
+for tweet in tweets:
+    processed_tweets.append(process_tweet(tweet))
+
+def get_people_names(words):
+    # input: array of strings
+	names = nltk.corpus.names
+	all_names = names.words('male.txt') + names.words('female.txt')
+	all_names = [name.lower() for name in all_names]
+
+	tweet_names= []
+
+	for i in range(0, len(words)):
+		if words[i].lower() in all_names:
+			first_name = words[i]
+			if i+1 < len(words):
+				last_name = words[i+1]
+				full_name = first_name.title() + " " + last_name.title()
+				if full_name in wikipedia.search(full_name) or full_name + " (actor)" in wikipedia.search(full_name):
+					tweet_names.append(full_name)
+			else:
+				tweet_names.append(first_name)
+	return tweet_names
 
 def get_movie_names_simple(tweet):
 	capitalized_words = []
@@ -67,8 +65,10 @@ def get_hosts(tweets):
     host_names = []
 
     for tweet in tweets:
-        if "host" in tweet or "Host" in tweet:
-            host_names.append(get_people_names(tweet))
+        for word in tweet:
+            if "host" in word or "Host" in word:
+                host_names.append(get_people_names(tweet))
+                break
 
     for group in host_names:
         for name in group:
@@ -76,5 +76,4 @@ def get_hosts(tweets):
 
     hosts = cnt.most_common(1)
     return hosts[0][0]
-
-print get_hosts(data[0:2000])
+print "host is", get_hosts(processed_tweets)
