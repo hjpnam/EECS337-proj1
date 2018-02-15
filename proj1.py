b52f5ba10d9d9830afe1c64bbf4840aa3b8c5a05
hjpnam <hjpnam@gmail.com>
2018-02-14 21:15:23 -0600

    get_winners added, not working code

--- proj1.py
+++ proj1.py
@@ -3,7 +3,8 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import json
 import re
-from difflib import SequenceMatcher
+from AwardCounter import *
+from results import *
 
 
 OFFICIAL_AWARDS = ['cecil b. demille award', 'best motion picture - drama', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best motion picture - comedy or musical', 'best performance by an actress in a motion picture - comedy or musical', 'best performance by an actor in a motion picture - comedy or musical', 'best animated feature film', 'best foreign language film', 'best performance by an actress in a supporting role in a motion picture', 'best performance by an actor in a supporting role in a motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best television series - comedy or musical', 'best performance by an actress in a television series - comedy or musical', 'best performance by an actor in a television series - comedy or musical', 'best mini-series or motion picture made for television', 'best performance by an actress in a mini-series or motion picture made for television', 'best performance by an actor in a mini-series or motion picture made for television', 'best performance by an actress in a supporting role in a series, mini-series or motion picture made for television', 'best performance by an actor in a supporting role in a series, mini-series or motion picture made for television']
@@ -16,31 +17,10 @@ def process_tweet(tweet):
 	#tweet = re.sub(r"#\S+", "", tweet)
 	#tweet = re.sub(r"@\S+", "", tweet)
 	word_tokens = word_tokenize(tweet)
-	stop_words = set(stopwords.words('english')) | set(["GoldenGlobes", 'goldenglobes', 'Goldenglobes', 'Golden','golden','globes','Globes', 'RT', 'I', "Oscars", "oscars", "Awards", "awards","!",",",".","?",":",';',"#","@"])
+	stop_words = set(stopwords.words('english')) | set(["GoldenGlobes", 'goldenglobes', 'Goldenglobes', 'Golden','golden','globes','Globes', 'RT', 'I', "Oscars", "oscars","!",",",".","?",';',"#","@"]) - set(['in','In','Out','out','by','By','for','For','From','from','over','Over','under','Under'])
 
 	filtered = [w for w in word_tokens if not w in stop_words]
-	return filtered
-
-def get_names(words):
-	names = nltk.corpus.names
-	all_names = names.words('male.txt') + names.words('female.txt')
-	all_names = [name.lower() for name in all_names]
-
-	tweet_names= []
-
-	for i in range(0, len(words)):
-		if words[i].lower() in all_names:
-			first_name = words[i]
-			if i+1 < len(words):
-				last_name = words[i+1]
-				full_name = first_name.title() + " " + last_name.title()
-				if full_name in wikipedia.search(full_name) or full_name + " (actor)" in wikipedia.search(full_name):
-					tweet_names.append(full_name)
-			else:
-				tweet_names.append(first_name)
-	return tweet_names
-
-
+	return ' '.join(filtered)
 
 def extract_award_tweets():
 	helper_words = set(['Drama', 'drama', 'Performance', 'performance', 'Best', 'best', 'Original', 'original', 'Screenplay', 'screenplay', 'Director', 'director', 'Role', 'role', 'Score', 'score', 'Song', 'song', 'actor', 'Actor', 'Actress', 'actress', 'Comedy','comedy', 'Musical', 'musical', 'Feature', 'feature', 'supporting','Supporting', 'Foreign', 'foreign', 'animated','Animated', 'Picture', 'picture', 'Motion', 'motion', 'Language', 'language', 'Director', 'director', 'Mini-series', 'mini-series', 'mini','Mini'])
@@ -54,6 +34,36 @@ def extract_award_tweets():
 
 	return award_tweets
 
+def get_winners(tweets):
+	presenters = AwardCounter()
+	winners = AwardCounter()
+	presenters_result = {}
+	winners_result = {}
+	nominees_result = {}
+	
+	for award in OFFICIAL_AWARDS:
+		presenters.add_award(award)
+		winners.add_award(award)
+		
+	for tweet in tweets:
+		for award in OFFICIAL_AWARDS:
+			if award in tweet:
+				proper_nouns = get_proper_nouns(tweet)
+				if "present" in tweet or "Present" in tweet:
+					for noun in proper_nouns:
+						presenters.increment(award,noun)
+				else:
+					for noun in proper_nouns:
+						winners.increment(award, noun)
+		
+	for award in OFFICIAL_AWARDS:
+		presenters_result[award] = presenters.get_max_actor(award)[0]
+		winners_result[award] = winners.get_max_actor(award)[0]
+		nominees_result[award] = winners.get_max_n_actors(award, 5)
+	for 
+	
+	return presenters_result, winners_result, nominees_result
+'''
 def extract_awards():
 	helper_words = ['Drama', 'drama', 'Performance', 'performance', 'Best', 'best', 'Original', 'original', 'Screenplay', 'screenplay', 'Director', 'director', 'Role', 'role', 'Score', 'score', 'Song', 'song', 'actor', 'Actor', 'Actress', 'actress', 'Comedy','comedy', 'Musical', 'musical', 'Feature', 'feature', 'supporting','Supporting', 'Foreign', 'foreign', 'animated','Animated', 'Picture', 'picture', 'Motion', 'motion', 'Language', 'language', 'Director', 'director', 'Mini-series', 'mini-series', 'mini','Mini','limited','Limited','Series','series','Cecil B. DeMille Award','cecil b. demille Award', 'Cecil B. DeMille']
 	
@@ -98,10 +108,5 @@ def extract_awards():
 			awards.remove(r)
 		
 	return awards
-
-
-	
-x = extract_awards()
-print (x)
-print (len(x))
+'''
 
