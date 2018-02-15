c45eb5881c6d794f745048d225d3c80f16915bfb
Hjpnam <hjpnam@gmail.com>
2018-02-14 18:04:50 -0600

    blah

--- proj1.py
+++ proj1.py
@@ -3,12 +3,8 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import json
 import re
-from sets import Set
 from difflib import SequenceMatcher
 import wikipedia
-import urllib2
-
-tmdb = json.load(urllib2.urlopen("https://api.themoviedb.org/3/discover/movie?api_key=2dc7ef3006499011242fcfbe603339e2&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&primary_release_year=2017&vote_average.gte=6.5"))
 
 
 OFFICIAL_AWARDS = ['cecil b. demille award', 'best motion picture - drama', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best motion picture - comedy or musical', 'best performance by an actress in a motion picture - comedy or musical', 'best performance by an actor in a motion picture - comedy or musical', 'best animated feature film', 'best foreign language film', 'best performance by an actress in a supporting role in a motion picture', 'best performance by an actor in a supporting role in a motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best television series - comedy or musical', 'best performance by an actress in a television series - comedy or musical', 'best performance by an actor in a television series - comedy or musical', 'best mini-series or motion picture made for television', 'best performance by an actress in a mini-series or motion picture made for television', 'best performance by an actor in a mini-series or motion picture made for television', 'best performance by an actress in a supporting role in a series, mini-series or motion picture made for television', 'best performance by an actor in a supporting role in a series, mini-series or motion picture made for television']
